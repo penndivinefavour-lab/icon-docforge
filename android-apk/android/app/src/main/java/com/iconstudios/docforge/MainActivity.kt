8ac4cde5f302087cd2329ed22dc12aad4775ec6a
Divine Favour (De ICON) <iconstudiosyde@gmail.com>
2026-09-25 00:04:19 +0100

    fix: simplify MainActivity to AppCompatActivity, add buildConfig feature

--- android-apk/android/app/src/main/java/com/iconstudios/docforge/MainActivity.kt
+++ android-apk/android/app/src/main/java/com/iconstudios/docforge/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.iconstudios.docforge
 
 import android.os.Bundle
-import android.webkit.WebView
-import com.getcapacitor.BridgeActivity
+import androidx.appcompat.AppCompatActivity
 import androidx.core.content.FileProvider
 import java.io.File
 
-class MainActivity : BridgeActivity() {
+class MainActivity : AppCompatActivity() {
     
     companion object {
         const val TAG = "IconDocForge"
@@ -17,7 +16,7 @@ class MainActivity : BridgeActivity() {
         
         // Enable WebView debugging in debug builds
         if (BuildConfig.DEBUG) {
-            WebView.setWebContentsDebuggingEnabled(true)
+            android.webkit.WebView.setWebContentsDebuggingEnabled(true)
         }
     }
     
@@ -39,40 +38,6 @@ class MainActivity : BridgeActivity() {
         } catch (e: Exception) {
             e.printStackTrace()
             null
-        }
-    }
-    
-    /**
-     * Share a file via Android share sheet
-     */
-    fun shareFile(filePath: String, mimeType: String) {
-        val uri = getFileUri(filePath) ?: return
-        
-        val intent = android.content.Intent(android.content.Intent.ACTION_SEND).apply {
-            type = mimeType
-            putExtra(android.content.Intent.EXTRA_STREAM, uri)
-            addFlags(android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION)
-        }
-        
-        startActivity(android.content.Intent.createChooser(intent, "Share via"))
-    }
-    
-    /**
-     * Open a file with appropriate app
-     */
-    fun openFile(filePath: String, mimeType: String) {
-        val uri = getFileUri(filePath) ?: return
-        
-        val intent = android.content.Intent(android.content.Intent.ACTION_VIEW).apply {
-            setDataAndType(uri, mimeType)
-            addFlags(android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION)
-            addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK)
-        }
-        
-        try {
-            startActivity(intent)
-        } catch (e: Exception) {
-            android.widget.Toast.makeText(this, "No app found to open this file type", android.widget.Toast.LENGTH_LONG).show()
         }
     }
 }
